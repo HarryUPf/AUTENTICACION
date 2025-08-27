a54523967cc184c4ccf2c310e0df8bb4003053af
HarryUP <harryup.dev@icloud.com>
2025-08-27 18:39:05 -0500

    entrypoint POST usuarios

---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/Handler.java
@@ -0,0 +1,34 @@
+package co.com.bancolombia.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class Handler {
+//private  final UseCase useCase;
+//private  final UseCase2 useCase2;
+
+    public Mono<ServerResponse> listenGETUseCase(ServerRequest serverRequest) {
+        // useCase.logic();
+        return ServerResponse.ok().bodyValue("listenGETUseCase");
+    }
+
+    public Mono<ServerResponse> listenGETOtherUseCase(ServerRequest serverRequest) {
+        // useCase2.logic();
+        return ServerResponse.ok().bodyValue("listenGETOtherUseCase");
+    }
+
+    public Mono<ServerResponse> listenPOSTUseCase(ServerRequest serverRequest) {
+        // useCase.logic();
+        return ServerResponse.ok().bodyValue("listenPOSTUseCase");
+    }
+
+    public Mono<ServerResponse> listenPOSTUsuariosUseCase(ServerRequest serverRequest) {
+        // useCase.logic();
+        return ServerResponse.ok().bodyValue("listenPOSTUsuariosUseCase");
+    }
+}
